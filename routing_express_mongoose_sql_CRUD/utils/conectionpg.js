8fe669e6c65f7ba2109ff2d979d3c0e0fd8e0ccf
silvialcastilla <silope0302@gmail.com>
2021-09-06 17:12:21 +0200

    data

--- routing_express_mongoose_sql_CRUD/utils/conectionpg.js
+++ routing_express_mongoose_sql_CRUD/utils/conectionpg.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.USERDB,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port:process.env.PORTPG,
+  user: 'gkngcqgn',
+  host: 'queenie.db.elephantsql.com',
+  database: 	'gkngcqgn',
+  password: 'w7DcftkSiVovwpG9nfRYCgOW8hcUguZI',
 })
+
+//postgres://gkngcqgn:w7DcftkSiVovwpG9nfRYCgOW8hcUguZI@queenie.db.elephantsql.com/gkngcqgn  
 
 console.log(process.env.USERITO)
 
