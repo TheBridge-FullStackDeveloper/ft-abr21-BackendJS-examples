ef7094085622f82a851fd1ef481824e297982312
silvialcastilla <silope0302@gmail.com>
2021-09-07 16:28:16 +0200

    data

--- routing_express_mongoose_sql_CRUD/utils/conectionpg.js
+++ routing_express_mongoose_sql_CRUD/utils/conectionpg.js
@@ -8,9 +8,6 @@ const pool = new Pool({
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
 })
-
-//postgres://gkngcqgn:w7DcftkSiVovwpG9nfRYCgOW8hcUguZI@queenie.db.elephantsql.com/gkngcqgn  
-
 console.log(process.env.USERITO)
 
   module.exports = pool
